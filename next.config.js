32ed396e291f2d0edc8b26ba94825ce3bf1fbf4c
Moiz Husnain <moiz@grandeur.tech>
2022-06-23 14:52:41 +0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// Setup nextra
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
 
-module.exports = nextConfig
+module.exports = withNextra()

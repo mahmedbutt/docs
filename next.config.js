fa061bade07162e51591c0e4d26baa71805cf870
Moiz Husnain <moiz@grandeur.tech>
2022-06-25 00:24:23 +0500

    converted into a plugin bug: pages aren't passing through _app

--- next.config.js
+++ next.config.js
@@ -1,43 +1,8 @@
-// Import packages
-const path = require("path");
+// Load our custom plugin to process mdx
+const withMDX = require("./lib/index");
 
-// Next config object
-const pluginOptions = {}
+// Provide it to next
+module.exports = withMDX({
+	// Next config
 
-// Return a custom webpack plugin
-// It gets the next config in parameter
-// We will return an object
-module.exports = (nextConfig) => ({
-	// With orignal config
-	...nextConfig,
-
-	// Add page extensions to support md and mdx files
-	pageExtensions: Array.from(
-		new Set([...(nextConfig.pageExtensions ?? []), "md", "mdx"])
-	),
-
-	// and return custom webpack plugin
-	webpack(config, options) {
-
-		// Add a rule to webpack 
-		config.module.rules.push({
-
-		// Test for md and mdx files
-		test: /\.mdx$/,
-		use: [
-			options.defaultLoaders.babel,
-			{
-				// And load custom parser
-				loader: path.join(__dirname, "/lib/loader.js"),
-				options: {...pluginOptions},
-			},
-		],
-		})
-
-		if (typeof nextConfig.webpack === 'function') {
-			return nextConfig.webpack(config, options)
-		}
-
-		return config
-	}
-})
+});

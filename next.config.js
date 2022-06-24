946363b7751e3bead33a2437a055d820c87c2184
Moiz Husnain <moiz@grandeur.tech>
2022-06-25 03:51:10 +0500

    Moved to the new implementation mdx is now running as a sidecart

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // Load our custom plugin to process mdx
-const withMDX = require("./lib/index");
+const withMDX = require("./lib/index")({ data: "data", out: "pages" });
 
 // Provide it to next
 module.exports = withMDX({

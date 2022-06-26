9fbe21796340f8ab9696ba1a2ac7971424bbee4f
Moiz Husnain <moiz@grandeur.tech>
2022-06-27 01:41:06 +0500

    Now wrapping the generated components with layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import Layout from '../components/layout';
+import routes from './layout.json';
+
+routes = routes.reverse();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout routes={routes} ><Component {...pageProps} /></Layout>
 }
 
 export default MyApp

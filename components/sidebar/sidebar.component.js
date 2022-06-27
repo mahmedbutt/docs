952f1c4c4da57514b65b77360858212b7d616641
Moiz Husnain <moiz@grandeur.tech>
2022-06-27 06:30:06 +0500

    Added sidebar Now support meta.json with plugin

--- components/sidebar/sidebar.component.js
+++ components/sidebar/sidebar.component.js
@@ -4,6 +4,22 @@
 // Libraries
 import React, { Component } from 'react';
 import Link from "next/link";
+import { useRouter } from 'next/router';
+import cuid from "cuid";
+
+// Functional component for route
+function Route({ title, path, depth }) {
+
+    // Get router
+    const router = useRouter();
+
+    // Determine active state
+    var active = router.asPath === path ? "text-white-1" : "text-white-2"
+
+    // Return the markdown
+    return <div key={cuid()} className={`${active} text-base py-2 cursor-pointer`} style={{ marginLeft: `${ depth * 10 }px` }}>{title}</div>
+
+}
 
 // Component
 class Sidebar extends Component {
@@ -13,7 +29,42 @@ class Sidebar extends Component {
         super(props)
 
         // Component State
-        this.state = {};
+        this.state = {
+            toc: []
+        };
+    }
+
+    // Get Props
+    static getDerivedStateFromProps(props,state) {
+
+        return {
+            toc: Array.isArray(props.toc) ? props.toc : []
+        }   
+    }
+
+    // Render TOC
+    toc(routes, depth) {
+
+        // Array to store jsx of routes
+        var processed = [];
+
+        // Loop over route
+        for (var route of routes) {
+
+            // If it is a file i.e. root node
+            // Then simply convert it into right format
+            if (route.meta) processed.push(<Route key={cuid()} title={route.meta.title} depth={depth} path={route.path} />)
+            else {
+                // If it is a directory, then we gotta do another
+                // pass to also tranform all the children
+                processed.push(<div key={cuid()} className="mt-5 text-white-2 text-base">{route.key}</div>)
+                processed.push(...this.toc(route.children, depth + 1));
+            }
+
+        }
+
+        // Return routes markdown
+        return processed;
     }
 
     // Render
@@ -25,7 +76,11 @@ class Sidebar extends Component {
             <div className={`bg-gray-2 px-6 py-12 w-full h-full box-border flex flex-col items-start`}>
 
                 {/* Logo */}
-                <Link href="/"><img src="/images/logo.svg" className="block w-auto h-6 cursor-pointer" alt="Grandeur"/></Link>
+                <Link href="/"><img src="/images/logo.svg" className="block w-auto h-6 cursor-pointer" alt="Grandeur Logo"/></Link>
+
+                {/* Render TOC */}
+                <div className='mt-10 overflow-x-hidden overflow-y-auto'>{this.toc(this.state.toc, 0)}</div>
+                
 
             </div>
         );
--- pages/_layout.js
+++ pages/_layout.js
@@ -3,6 +3,9 @@ import Grid from '../components/grid/grid.component';
 import Container from '../components/container/container.component';
 import Sidebar from '../components/sidebar/sidebar.component';
 
+// Get sitemap
+import routes from './_data/layout.json';
+
 // Export component
 export default function Layout({children}) {
 
@@ -12,7 +15,7 @@ export default function Layout({children}) {
         <Grid columns={[{ xs: 0, sm: 3, md: 2, lg: 2 }, { xs: 12, sm: 9, md: 10, lg: 10 }]} pass={["", "overflow-x-hidden overflow-y-auto"]} height="full">
 
             {/* The first column is about the menu */}
-            <Sidebar column={0} />
+            <Sidebar column={0} toc={routes} />
 
             {/* Second column is about the content that we want to render */}
             <Container column={1} >
